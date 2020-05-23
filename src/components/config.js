861423d42343d57da07fefc51913794a31e21dd6
Cuong Duy Nguyen <103cuong@gmail.com>
2020-05-23 11:05:30 +0700

    💩 Clean unused source (#34)

--- src/components/config.js
+++ src/components/config.js
@@ -1,7 +1,7 @@
 export default {
   port: process.env.PORT || 9000,
   jwt: {
-    secretKey: process.env.SECRET_KEY || 'node_boilerplate',
+    secretKey: process.env.SECRET_KEY || 'csjhgt2gh',
     algorithm: process.env.ALGORITHM || 'HS256',
     expiresIn: process.env.EXPIRES_IN || '30m',
   },
