d73a58cbae6f29c47d94f5ba4e63815a7a98a678
Cuong Duy Nguyen <103cuong@gmail.com>
2020-06-30 15:18:35 +0700

    Update index.js

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -5,7 +5,7 @@ import { resolvers as scalarResolvers } from 'graphql-scalars';
 const resolvers = [scalarResolvers];
 fs
   .readdirSync(__dirname)
-  .filter((fileName) => /resolver.js$/.test(fileName))
+  .filter((fileName) => /resolver.js/.test(fileName))
   .forEach((fileName) => {
     const resolver = require(path.join(__dirname, fileName));
     resolvers.push(resolver.default);
