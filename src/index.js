2ac6c82f1649405eff8137c4d4239b3cc5318687
Cuong Duy Nguyen <103cuong@gmail.com>
2020-08-09 22:37:06 +0700

    feat: integrate juno-js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import path from 'path';
-import signale from 'signale';
+import { logger, globalOptions } from 'juno-js';
+
 import { migrateDB } from './components/utils';
 import db from './models';
 import app from './app';
+import config from './components/config';
+
+globalOptions.environment = config.nodeEnv;
 
 const main = async () => {
   const pathToMigration = path.join(__dirname, 'migrations');
-  await migrateDB(db.sequelize, pathToMigration).catch(signale.watch);
+  await migrateDB(db.sequelize, pathToMigration).catch(logger().error);
   app();
 };
 
-main().catch(signale.watch);
+main().catch(logger().error);
